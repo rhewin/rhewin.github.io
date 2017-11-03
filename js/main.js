79cd9e7d3a486ddf0ad188cb4b16cb06498befc1
Rheza Sindhuwinata <rheza@vi8e.com>
2017-11-03 22:25:35 +0700

    Update portfolio

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(function () {	
+	$(".backpanel").mCustomScrollbar({		
+		theme: "minimal",	
+		alwaysShowScrollbar: 0,
+	});	
+	
 	function close() {		
 		$('.backpanel').removeClass('active');	
 		$('.floatmenu li').removeClass('active');		
@@ -8,14 +13,9 @@ $(function () {
 			'right': 'auto'
 		})
 	}
-		
-	$(".backpanel").mCustomScrollbar({		
-		theme: "minimal-dark",	
-		alwaysShowScrollbar: true,
-	});	
 	
-	$('.link_about').click(function(){			
-		if ($('.panel_about').hasClass('active')) {			
+	function togglePanel(panel, link) {
+		if ($(panel).hasClass('active')) {			
 			$('.main_profile').css({
 				'left': '50%',
 				'margin-left': '-25%'
@@ -28,102 +28,39 @@ $(function () {
 				'left': '50%',
 				'right': 'auto'
 			});					
-			$('.panel_about').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e){
-				$('.panel_about').removeClass('active');
-				$('.link_about').removeClass('active');
+			$(panel).on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e){
+				$(panel).removeClass('active');
+				$(link).removeClass('active');
 				$(this).off(e);
 			});
 		} else {			
 			close();			
-			$('.panel_about').addClass('active');
-			$('.link_about').addClass('active');
+			$(panel).addClass('active');
+			$(link).addClass('active');
 			$('.main_profile, .backpanel').css({
-				'left': '0',
+				'left': '20px',
 				'margin-left': '108px'
 			});
 			$('.floatmenu').css({
-				'left': '0',
+				'left': '20px',
 			});
-			$('.panel_about').css({
+			$(panel).css({
 				'margin-left': '52px',	
 				'left': '50%',
 				'z-index': '6' 
 			});					
 		}		
+	}
+	
+	$('.link_about').click(function(){			
+		togglePanel('.panel_about', '.link_about');		
 	});	
 	
 	$('.link_resume').click(function(){			
-		if ($('.panel_resume').hasClass('active')) {			
-			$('.main_profile').css({
-				'left': '50%',
-				'margin-left': '-25%'
-			});
-			$('.floatmenu').css({
-				'left': 'calc(25% - 108px)',
-			});	
-			$('.backpanel').css({
-				'margin-left': '-25%',		
-				'left': '50%',				
-				'right': 'auto'
-			});					
-			$('.panel_resume').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e){
-				$('.panel_resume').removeClass('active');
-				$('.link_resume').removeClass('active');
-				$(this).off(e);
-			});
-		} else {
-			close();				
-			$('.panel_resume').addClass('active');
-			$('.link_resume').addClass('active');			
-			$('.main_profile, .backpanel').css({
-				'left': '0',
-				'margin-left': '108px'
-			});
-			$('.floatmenu').css({
-				'left': '0',
-			});
-			$('.panel_resume').css({
-				'margin-left': '52px',	
-				'left': '50%',				
-				'z-index': '6'
-			});		
-		}		
+		togglePanel('.panel_resume', '.link_resume');		
 	});	
-	$('.link_portfolio').click(function(){			
-		if ($('.panel_portfolio').hasClass('active')) {			
-			$('.main_profile').css({
-				'left': '50%',
-				'margin-left': '-25%'
-			});
-			$('.floatmenu').css({
-				'left': 'calc(25% - 108px)',
-			});	
-			$('.backpanel').css({
-				'margin-left': '-25%',
-				'left': '50%',						
-				'right': 'auto'
-			});					
-			$('.panel_portfolio').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e){
-				$('.panel_portfolio').removeClass('active');
-				$('.link_portfolio').removeClass('active');
-				$(this).off(e);
-			});
-		} else {
-			close();				
-			$('.panel_portfolio').addClass('active');
-			$('.link_portfolio').addClass('active');			
-			$('.main_profile, .backpanel').css({
-				'left': '0',
-				'margin-left': '108px'
-			});
-			$('.floatmenu').css({
-				'left': '0',
-			});
-			$('.panel_portfolio').css({
-				'margin-left': '52px',			
-				'left': '50%',				
-				'z-index': '6'
-			});		
-		}		
+	
+	$('.link_portfolio').click(function(){		
+		togglePanel('.panel_portfolio', '.link_portfolio');		
 	});		
 });
